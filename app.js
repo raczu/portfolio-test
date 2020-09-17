b355faf87077cc3cd4585177d3720e69147f4845
Dawid Machoczek <61229912+raczu@users.noreply.github.com>
2020-09-17 19:39:43 +0200

    Add files via upload
    upload

---
+++ app.js
@@ -0,0 +1,40 @@
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+
+  const up_arrow = document.querySelector("#up-arrow");
+
+  up_arrow.addEventListener("click", function() {
+      window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth"
+      });
+  });
+
+  element.scroll({ top: 0, left: 0, behavior: 'smooth' });
